f6032342721851c501770cbdcfa997fef026ff35
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-10-04 12:40:17 -0400

    Replaced MovieDetails with MovieCard - Stretch

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -25,19 +25,23 @@ const MovieList = props => {
     <div className="movie-list">
       {movies.map(movie => (
         <Link to={`/movies/${movie.id}`}>
-          <MovieDetails key={movie.id} movie={movie} />
+          {/* <MovieDetails key={movie.id} movie={movie} /> */}
+          <MovieCard key={movie.id} movie={movie}
+               title={movie.title} director={movie.director} 
+               metascore={movie.metascore} stars={movie.stars} 
+               saveMovie={movie.saveMovie}  />
         </Link>
       ))}
     </div>
   );
 }
 
-function MovieDetails({ movie }) {
-  return (
-    <MovieCard title={movie.title} director={movie.director} 
-               metascore={movie.metascore} stars={movie.stars} 
-               saveMovie={movie.saveMovie}  />
-  );
-}
+// function MovieDetails({ movie }) {
+//   return (
+//     <MovieCard title={movie.title} director={movie.director} 
+//                metascore={movie.metascore} stars={movie.stars} 
+//                saveMovie={movie.saveMovie}  />
+//   );
+// }
 
 export default MovieList;
